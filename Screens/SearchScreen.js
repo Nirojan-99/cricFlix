a75cc81a3e5420d79afedb3d664b27057ca686ed
Nirojan21 <nirojan.yoga@gmail.com>
2022-09-16 12:54:41 +0530

    finish search screen

--- Screens/SearchScreen.js
+++ Screens/SearchScreen.js
@@ -1,10 +1,20 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, Text, TextInput} from 'react-native';
+import React from 'react';
 
 export default function SearchScreen() {
   return (
-    <View>
-      <Text>SearchScreen</Text>
+    <View className=" flex-1">
+      <View className="mx-5 mt-14">
+        <Text className="text-[#031952] text-center">
+          <Text className="font-bold">SEARCH </Text>
+          for Fixtures, Videos, Podcast or NFT
+        </Text>
+        <TextInput
+          className="border-b border-[#031952] mt-3 mx-5 text-gray-600 font-bold"
+          placeholder="Search for"
+          placeholderTextColor={'#333'}
+        />
+      </View>
     </View>
-  )
+  );
 }
